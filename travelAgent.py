82ea1333ab04bc5d3561e0ff46d83014f5cdeeaa
Gustavo Schmid de Jesus <gutors@Gustavos-Laptop.local>
2024-07-12 18:04:52 +0700

    Second day of nlw development
    - Added a supervisor agent to update the research agent response;
    
    - Utilizes the concept of RAG - Retrieval Augmented Generation by researching data from an external website and asking the supervisor to act as a trip manager to create an itinerary based on the response of the research agent and the data of the external website;

--- travelAgent.py
+++ travelAgent.py
@@ -1,8 +1,17 @@
 import os
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.agent_toolkits.load_tools import load_tools
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain import hub
+
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores.chroma import Chroma
+import bs4
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableSequence
+
 
 llm = ChatOpenAI(
     model="gpt-3.5-turbo",
@@ -16,22 +25,59 @@ llm = ChatOpenAI(
     # other params...
 )
 
-tools = load_tools(['ddg-search', 'wikipedia'], llm = llm)
-
-prompt = hub.pull("hwchase17/react")
-
-# print(tools[0].name, tools[0].description)
-# print(tools[1].name, tools[1].description)
-
-# create a reAct agent (reason and act)
-agent = create_react_agent(llm, tools, prompt)
-
-agentExecutor = AgentExecutor(agent=agent, tools=tools, prompt=prompt, verbose='true')
-
-#print(agent.agent.llm_chain.prompt.template)
-
 query="""
-Vou viajar para Asia em Agosto de 2024. Quero que faça um roteiro com os eventos que vao ocorrer na data da viagem e o preco das passagens de Sao Paulo para Tailandia.
+Vou viajar para Inglaterra em Agosto de 2024. Quero que faça um roteiro com os eventos que vao ocorrer na data da viagem e o preco das passagens de Sao Paulo para Londres.
 """
 
-agentExecutor.invoke({"input":query})
+def researchAgent(query: str, llm: ChatOpenAI):
+    tools = load_tools(['ddg-search', 'wikipedia'], llm = llm)
+    prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm, tools, prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, prompt=prompt, verbose='true')
+    webContext = agent_executor.invoke({"input":query})
+    return webContext['output']
+
+def loadData():
+    loader = WebBaseLoader(
+        web_path="https://dicasdeviagem.com/inglaterra",
+        bs_kwargs=dict(parse_only=bs4.SoupStrainer(class_=("postcontentwrap", "pagetitleloading background-imaged loading-dark"))),
+    )
+    docs = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    docs = text_splitter.split_documents(docs)
+    vector_store = Chroma.from_documents(docs, embedding=OpenAIEmbeddings())
+    retriever = vector_store.as_retriever()
+    return retriever
+
+def getRelevantDocs(query: str):
+    retriever = loadData()
+    relevant_docs = retriever.invoke(query)
+    print(relevant_docs)
+    return relevant_docs
+
+def supervisorAgent(query: str, llm: ChatOpenAI, webContext, relevantDocuments):
+    prompt_template = """
+    Você é um gerente de uma agência de viagens. Sua resposta final deverá ser um roteiro de viagem completo e detalhado. 
+    Utilize o contexto de eventos e preços de passagens, o input do usuário e também os documentos relevantes para elaborar o roteiro.
+    Contexto: {webContext}
+    Documento relevante: {relevant_documents}
+    Usuário: {query}
+    Assistente:
+    """
+
+    prompt = PromptTemplate(
+        input_variables=["webContext", "relevant_documents", "query"],
+        template=prompt_template,
+    )
+
+    sequence = RunnableSequence(prompt | llm)
+
+    return sequence.invoke({"webContext": webContext, "relevant_documents": relevantDocuments, "query": query})
+
+def getResponse(query: str, llm):
+    webContext = researchAgent(query, llm)
+    relevantDocuments = getRelevantDocs(query)
+    response = supervisorAgent(query, llm, webContext, relevantDocuments)
+    return response
+
+print (getResponse(query, llm))
